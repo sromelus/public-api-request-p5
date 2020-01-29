c70b100a131340efd2e3bcaf996a91263daefcc1
sromelus <46148909+sromelus@users.noreply.github.com>
2020-01-29 15:52:55 -0500

    update api endpoint

--- js/card.js
+++ js/card.js
@@ -1,4 +1,4 @@
-fetch("https://randomuser.me/api/?results=12")
+fetch("https://randomuser.me/api/?nat=us&results=12")
 .then(response => response.json())
 .then(data => generateProfiles(data.results))
 
